2fd3fe33516bdb7e557158e24d3850a9df5fa39f
Dushime20 <fabdushime20@gmail.com>
2025-07-01 11:03:20 +0200

    correct padding and margin

--- src/home/layout/Footer.jsx
+++ src/home/layout/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-br from-blue-50 via-white to-blue-100 text-blue-900 pt-16 pb-8 border-t border-blue-100 mt-16">
+    <footer className="bg-gradient-to-br from-blue-50 via-white to-blue-100 text-blue-900 pt-4 pb-8 border-t border-blue-100">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mb-10">
           {/* About Us */}
